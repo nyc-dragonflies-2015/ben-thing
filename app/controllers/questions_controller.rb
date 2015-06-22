8415338122fa0215b73abebfb05bdff74789bdaa
Steven G. Harms <steven@devbootcamp.com>
2015-06-22 17:48:33 -0400

    Fill out QuestionsController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,24 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      render text: 'aw yah'
+    else
+      render text: 'die in a fire'
+    end
   end
+
+  private
+
+    def question_params
+      params.require(:question).permit(:body, :survey_id)
+    end
 end
